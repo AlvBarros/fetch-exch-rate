ef087d62fa18ddd5fdb3d0cf1d4035cc54feb8c4
AlvBarros <alv.barrosc@hotmail.com>
2022-12-06 17:42:17 -0300

    feat: :bookmark: 1.0.0

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,10 +2,17 @@ import { Controller, Get } from "@nestjs/common";
 import { HealthcheckService } from "./healthcheck/healthcheck.service";
 import { Diagnostics } from "./healthcheck/interfaces/diagnostics";
 
+const url = 'https://github.com/AlvBarros/fetch-exch-rate'
+
 @Controller()
 export class AppController {
   constructor(private readonly healthcheckService: HealthcheckService) {}
 
+  @Get()
+  async helloWorld(): Promise<string> {
+    return `Hello World! This is hosting ${url}`
+  }
+  
   @Get('/health')
   async getHealthcheck() : Promise<Diagnostics> {
     return this.healthcheckService.getDiagnostics();
